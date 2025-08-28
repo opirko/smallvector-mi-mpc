1802a9096617c1c5163c4022963736c8c4ef76b6
Ondrej Pirko <ondra@pirko.cz>
2025-08-28 14:18:25 +0200

    move to cmake

--- src/testMain.cpp
+++ src/testMain.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-#include "src/smallVector.hpp"
+#include "smallVector.hpp"
 
-int main(int argc, char** argv) {
+int main(int, char**) {
   std::cout << "Test Main start." << std::endl;
   mpc::smallVector<int> vec(5);
   std::cout << "Test Main end." << std::endl;
